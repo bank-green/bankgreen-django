667b1034bfbee4fbd241726576eaddc06330b0a2
Albert Carter <albert.r.carter@gmail.com>
2022-02-06 15:18:31 +0100

    add comentary to brand view. update commentary models

--- brand/admin.py
+++ brand/admin.py
@@ -1,11 +1,59 @@
 from django.contrib import admin
 
-from .models import Brand
+from .models import Brand, Commentary
+
+
+class CommentaryInline(admin.StackedInline):
+    model = Commentary
+    fieldsets = (
+        (
+            'Display Configuration',
+            {
+                'fields': (
+                    ('display_on_website', 'aliases'),
+                    ('rating', 'top_three_ethical'),
+                    ('recommended_in', 'recommended_order'),
+                )
+            },
+        ),
+        (
+            'Text used for both positively and negatively rated banks',
+            {'fields': ('headline', 'top_blurb_headline', 'top_blurb_subheadline')},
+        ),
+        (
+            'Text used for negatively rated banks',
+            {
+                'fields': (
+                    ('snippet_1', 'snippet_1_link'),
+                    ('snippet_2', 'snippet_2_link'),
+                    ('snippet_3', 'snippet_3_link'),
+                )
+            },
+        ),
+        (
+            'Text used for positively rated banks',
+            {
+                'fields': (
+                    ('from_the_website',),
+                    ('checking_saving_details', 'checking_saving'),
+                    ('free_checking_details', 'free_checking'),
+                    ('interest_rates',),
+                    ('free_atm_withdrawl_details', 'free_atm_withdrawl'),
+                    ('local_branches_details', 'local_branches', 'online_banking'),
+                    ('mortgage_or_loan', 'deposit_protection'),
+                    ('credit_cards_details', 'credit_cards'),
+                    ('free_international_card_payment',),
+                )
+            },
+        ),
+        ('Meta', {'fields': ('comment',)}),
+    )
 
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     list_display = ['name', 'tag', 'website']
+    list_filter = ('date_updated', 'countries')
     search_fields = ['name', 'tag', 'website']
     fields = (
         ('name', 'tag'),
@@ -18,6 +66,7 @@ class BrandAdmin(admin.ModelAdmin):
         ('subsidiary_of_4', 'subsidiary_of_4_pct'),
         ('date_added', 'date_updated'),
     )
+    inlines = (CommentaryInline,)
 
     def get_queryset(self, request):
         # filter out all but base class
--- brand/models/commentary.py
+++ brand/models/commentary.py
@@ -7,97 +7,118 @@ from brand.models import Brand
 
 class Commentary(models.Model):
     # Metadata
-    brand = models.ForeignKey(
+    brand = models.OneToOneField(
         Brand,
         related_name="commentary_brand",
         help_text="What brand is this comment associated with?",
-        null=False,
-        blank=True,
         on_delete=models.CASCADE,
     )
     aliases = models.CharField(
-        "Other names for the brand, comma seperated. i.e. BOFA, BOA", max_length=200, null=True, blank=True
-    )
-    display_on_website = models.BooleanField(default=False)
-    comment = models.TextField("Meta. Comments for staff and/or editors")
-    rating = models.CharField('Rating', max_length=6, null=False, blank=False)
-
-    # Neutral Commentary
-    unique_statement = models.TextField(
-        "Positive/Negative. i.e. Despite introducing policies to restrict unconventional oil and gas finance, BNP Paribas recently ",
+        help_text="Other names for the brand, used for search. comma seperated. i.e. BOFA, BOA",
+        max_length=200,
         null=True,
         blank=True,
     )
-    headline = models.CharField("Positive/Negative. i.e. #1 in Coal", max_length=200, null=True, blank=True)
+    display_on_website = models.BooleanField(default=False)
+    comment = models.TextField(help_text="Meta. Comments for staff and/or editors")
+    rating = models.CharField(max_length=6, null=False, blank=False)
 
-    top_blurb_headline = models.TextField(
-        "Positive/Negative. i.e. Your money is being used to fund the climate crisis at an alarming rate."
+    # Neutral Commentary
+    unique_statement = models.CharField(
+        help_text="Positive/Negative. i.e. Despite introducing policies to restrict unconventional oil and gas finance, BNP Paribas recently ",
+        null=True,
+        max_length=300,
+        blank=True,
     )
-    top_blurb_subheadline = models.TextField(
-        "Positive/Negative. i.e. According to the latest research*, in 2020 your bank was the 4th largest funder..."
+    headline = models.CharField(help_text="Positive/Negative. i.e. #1 in Coal", max_length=200, null=True, blank=True)
+
+    top_blurb_headline = models.CharField(
+        help_text="Positive/Negative. i.e. Your money is being used to fund the climate crisis at an alarming rate.",
+        max_length=200,
+    )
+    top_blurb_subheadline = models.CharField(
+        help_text="Positive/Negative. i.e. According to the latest research*, in 2020 your bank was the 4th largest funder...",
+        max_length=300,
     )
 
     # Negative Commentary
-    snippet_1 = models.TextField(
-        "Negative. Custom fact about the brand.", help_text="Used to fill in templates", blank=True, default=''
+    snippet_1 = models.CharField(
+        max_length=150,
+        help_text="Negative. Custom fact about the brand. Used to fill in templates",
+        blank=True,
+        default='',
     )
-    snippet_1_link = models.URLField("link to dirty deal 1 detauls", blank=True, default='')
-    snippet_2 = models.TextField(
-        "Negative. Custom fact about the brand.", help_text="Used to fill in templates", blank=True, default=''
+    snippet_1_link = models.URLField(help_text="link to dirty deal 1 detauls", blank=True, default='')
+    snippet_2 = models.CharField(
+        max_length=150,
+        help_text="Negative. Custom fact about the brand. Used to fill in templates",
+        blank=True,
+        default='',
     )
-    snippet_2_link = models.URLField("link to dirty deal 2 detauls", blank=True, default='')
-    snippet_3 = models.TextField(
-        "Negative. Custom fact about the brand.", help_text="Used to fill in templates", blank=True, default=''
+    snippet_2_link = models.URLField(help_text="link to dirty deal 2 detauls", blank=True, default='')
+    snippet_3 = models.CharField(
+        max_length=150,
+        help_text="Negative. Custom fact about the brand. Used to fill in templates",
+        blank=True,
+        default='',
     )
-    snippet_3_link = models.URLField("link to dirty deal 3 detauls", blank=True, default='')
+    snippet_3_link = models.URLField(help_text="link to dirty deal 3 detauls", blank=True, default='')
 
     # Positive Commentary
-    top_three_ethical = models.BooleanField("Positive. Is this bank a top three bank?", default=False)
+    top_three_ethical = models.BooleanField(
+        help_text="Positive. Is this bank recommended best banks of a country page?", default=False
+    )
     recommended_order = models.IntegerField(
-        "Positive. in case there are more recommended banks than can fit on the page, lower numbers are given priority",
+        help_text="Positive. in case there are more recommended banks than can fit on the page, lower numbers are given priority",
         default=3,
         null=True,
     )
     recommended_in = CountryField(multiple=True, help_text="Positive. what countries will this bank be recommended in?")
-    from_the_website = models.TextField('Positive. used to to describe green banks in their own words')
+    from_the_website = models.TextField(help_text='Positive. used to to describe green banks in their own words')
 
     # Banking Detail Inforamtion, mostly used for recommended banks
-    checking_saving = models.BooleanField("Positive. does the bank offer checkings or savings accounts?", default=False)
+    checking_saving = models.BooleanField(
+        help_text="Positive. does the bank offer checkings or savings accounts?", default=False
+    )
     checking_saving_details = models.CharField(
-        "Positive. Details on available checkings and savings accounts", max_length=100, blank=True, default=''
+        help_text="Positive. Details on available checkings and savings accounts", max_length=50, blank=True, default=''
     )
 
-    free_checking = models.BooleanField("Positive. does the bank offer free checkings?", default=False)
+    free_checking = models.BooleanField(help_text="Positive. does the bank offer free checkings?", default=False)
     free_checking_details = models.CharField(
-        "Positive. Details on available free checkings", max_length=100, blank=True, default=''
+        help_text="Positive. Details on available free checkings", max_length=50, blank=True, default=''
     )
 
     interest_rates = models.CharField(
-        "Positive. Details about offered interest rates", max_length=100, null=True, blank=True
+        help_text="Positive. Details about offered interest rates", max_length=50, null=True, blank=True
     )
 
-    free_atm_withdrawl = models.BooleanField("Positive. does the bank offer free ATM withdrawals?", default=False)
+    free_atm_withdrawl = models.BooleanField(
+        help_text="Positive. does the bank offer free ATM withdrawals?", default=False
+    )
     free_atm_withdrawl_details = models.CharField(
-        "Positive. Details on available free ATM withdrawals", max_length=100, blank=True, default=''
+        help_text="Positive. Details on available free ATM withdrawals", max_length=50, blank=True, default=''
     )
 
-    online_banking = models.BooleanField("Positive. does the bank offer online banking?", default=False)
-    local_branches = models.BooleanField("Positive. does the bank offer local branches?", default=False)
-    local_branhes_custom = models.CharField(
-        "Positive. Details on local branches", max_length=100, null=True, blank=True
+    online_banking = models.BooleanField(help_text="Positive. does the bank offer online banking?", default=False)
+    local_branches = models.BooleanField(help_text="Positive. does the bank offer local branches?", default=False)
+    local_branches_details = models.CharField(
+        help_text="Positive. Details on local branches", max_length=50, null=True, blank=True
     )
 
-    mortgage_or_loan = models.BooleanField("Positive. does the bank offer mortgage or loans?", default=False)
+    mortgage_or_loan = models.BooleanField(help_text="Positive. does the bank offer mortgage or loans?", default=False)
     deposit_protection = models.CharField(
-        "Positive. Details on deposit protection", max_length=100, null=True, blank=True
+        help_text="Positive. Details on deposit protection", max_length=50, null=True, blank=True
     )
 
-    credit_cards = models.BooleanField("Positive. does the bank offer credit cards?", default=False)
-    credit_cards_details = models.CharField("Positive. Details on credit cards", max_length=100, null=True, blank=True)
+    credit_cards = models.BooleanField(help_text="Positive. does the bank offer credit cards?", default=False)
+    credit_cards_details = models.CharField(
+        help_text="Positive. Details on credit cards", max_length=50, null=True, blank=True
+    )
 
     free_international_card_payment = models.BooleanField(
-        "Positive. does the bank offer free international card payments?", default=False
+        help_text="Positive. does the bank offer free international card payments?", default=False
     )
     free_international_card_payment_details = models.CharField(
-        "Positive. Details on free international card payments", max_length=100, null=True, blank=True
+        help_text="Positive. Details on free international card payments", max_length=50, null=True, blank=True
     )
