0e1ca110421789e9d42dca918666a69fbfa0b223
Albert R Carter <albert.r.carter@gmail.com>
2022-04-26 17:43:07 +0200

    add usnic and wikidata for tag_prepend_str

--- datasource/models/usnic.py
+++ datasource/models/usnic.py
@@ -5,8 +5,12 @@ from django.db import models
 
 import requests
 
-from datasource.models.datasource import Datasource
+from datasource.models.datasource import Datasource, classproperty
 
 
 class Usnic(Datasource):
     """ """
+
+    @classproperty
+    def tag_prepend_str(cls):
+        return cls.__name__.lower() + "_"
--- datasource/models/wikidata.py
+++ datasource/models/wikidata.py
@@ -1,12 +1,9 @@
-import json
-from datetime import datetime, timezone
-
-from django.db import models
-
-import requests
-
-from datasource.models.datasource import Datasource
+from datasource.models.datasource import Datasource, classproperty
 
 
 class Wikidata(Datasource):
     """ """
+
+    @classproperty
+    def tag_prepend_str(cls):
+        return cls.__name__.lower() + "_"
