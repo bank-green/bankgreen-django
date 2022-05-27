0d6409358d55ce10282febd695f04c0efa4b7257
Simon <abeluga@posteo.net>
2022-05-27 13:41:14 +0200

    change query param name to be more apt

--- brand/schema.py
+++ brand/schema.py
@@ -31,7 +31,7 @@ class BrandFilter(FilterSet):
         return queryset.filter(countries__contains=value)
 
     rating = MultipleChoiceFilter(field_name="commentary__rating", choices=RatingChoice.choices)
-    top_three_ethical = BooleanFilter(field_name="commentary__top_three_ethical")
+    recommended_only = BooleanFilter(field_name="commentary__top_three_ethical")
 
     class Meta:
         model = Brand
