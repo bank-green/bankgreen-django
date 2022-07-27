2b8e07f806ca5f086231c382bccf6ae7d835010b
Albert Carter <albert.r.carter@gmail.com>
2022-07-27 14:00:43 +0200

    Features (#59)
    * add fossil fuel alliance checkbox and display in admin
    
    * migrate hardcoded features to more generic model.
    
    brand/schema.py still needs work
    
    * fix import
    
    * add graphql support for bank_features
    
    * remove applicable countries from feature. Update availability choices
    
    * remove applicable_countries from admin view
    
    * make feature implementations unique
    
    Co-authored-by: Simon <abeluga@posteo.net>

--- brand/models/features.py
+++ brand/models/features.py
@@ -1,8 +1,11 @@
 from django.db import models
 from brand.models import Brand
+from django_countries.fields import CountryField
 
 
 class Features(models.Model):
+    """DEPRECATED. SHOULD BE REMOVED IN NEXT MIGRATION"""
+
     brand = models.OneToOneField(
         Brand,
         related_name="features",
@@ -79,3 +82,46 @@ class Features(models.Model):
         null=True,
         blank=True,
     )
+
+
+class FeatureType(models.Model):
+    name = models.CharField(
+        max_length=40, help_text="i.e. Free Checking account, Credit Card, etc."
+    )
+    description = models.TextField(help_text="Description about this feature type")
+
+    def __str__(self):
+        return self.name
+
+
+class FeatureAvailabilityChoice(models.TextChoices):
+    YES = "Yes"
+    NO = "No"
+    MAYBE = "Maybe"
+    NOT_APPLICABLE = "N/A"
+
+
+class BrandFeature(models.Model):
+    brand = models.ForeignKey(
+        Brand, related_name="bank_features", null=False, blank=False, on_delete=models.CASCADE
+    )
+
+    feature = models.ForeignKey(FeatureType, null=False, blank=False, on_delete=models.CASCADE)
+
+    offered = models.CharField(
+        max_length=16,
+        choices=FeatureAvailabilityChoice.choices,
+        default=FeatureAvailabilityChoice.NOT_APPLICABLE,
+        help_text="Is the feature offered?",
+    )
+
+    details = models.CharField(
+        max_length=100, null=True, blank=True, help_text="Details about the feature"
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["feature", "brand"], name="unique_feature_implementation_per_bank"
+            )
+        ]
