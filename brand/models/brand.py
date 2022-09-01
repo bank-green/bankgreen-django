db4d1aa076b3d26d9c5a43f4adf9217cb3e1f6ba
Albert R Carter <al@albertrcarter.com>
2022-09-01 16:09:05 -0400

    validate that all regions have associated countries

--- brand/models/brand.py
+++ brand/models/brand.py
@@ -2,10 +2,10 @@ from typing import List, Tuple
 
 from django.db import models
 from django.template.defaultfilters import truncatechars
-from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-from django_countries.fields import CountryField
 from cities_light.models import Region
+from django_countries.fields import CountryField
 from model_utils.models import TimeStampedModel
 
 import datasource.models as dsm
