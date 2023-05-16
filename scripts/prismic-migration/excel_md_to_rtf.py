9fe32a82e04c35a6985551bf7aead2bca4f32bb2
Albert Carter <albert.r.carter@gmail.com>
2023-05-16 16:51:43 +0200

    add some helpful but chaotic migration scripts

---
+++ scripts/prismic-migration/excel_md_to_rtf.py
@@ -0,0 +1,86 @@
+import openpyxl
+import markdown
+import pyth.plugins.rtf15.reader
+import pyth.plugins.plaintext.writer
+
+# Load the Excel file
+workbook = openpyxl.load_workbook("commentaries.xlsx")
+sheet = workbook.active
+
+# Iterate through each cell in the sheet
+for row in sheet.iter_rows():
+    for cell in row:
+        # Extract the Markdown content
+        try:
+            markdown_content = cell.value if cell.value else ""
+            html_content = markdown.markdown(markdown_content)
+        except Exception as e:
+            print(markdown_content)
+            import pdb
+
+            pdb.set_trace()
+        # Convert Markdown to HTML
+        # Convert HTML to RTF
+        # doc = pyth.plugins.rtf15.reader.Rtf15Reader.read(html_content)
+        # rtf_content = pyth.plugins.plaintext.writer.PlaintextWriter.write(doc).getvalue()
+        # Update the cell value with RTF content
+        cell.value = html_content
+
+# Save the modified workbook
+workbook.save("output.xlsx")
+
+
+#
+
+
+import openpyxl
+import html2text
+
+# Load the Excel file
+workbook = openpyxl.load_workbook("output.xlsx")
+sheet = workbook.active
+
+# HTML content
+html_content = "<h1>Hello, World!</h1><p>This is <strong>HTML</strong> content.</p>"
+
+# Convert HTML to plain text
+plain_text = html2text.html2text(html_content)
+
+# Get the active cell
+cell = sheet.active_cell
+
+# Write the plain text content to the cell
+cell.value = plain_text
+
+# Apply formatting to the cell
+cell.font = openpyxl.styles.Font(bold=True, italic=True, underline="single")
+
+# Save the modified workbook
+workbook.save("output2.xlsx")
+
+
+#######
+
+import openpyxl
+
+# Load the Excel file
+workbook = openpyxl.load_workbook("output.xlsx")
+
+# Create a new workbook in write-only mode
+output_workbook = openpyxl.Workbook(write_only=True)
+
+# Iterate through each sheet in the input workbook
+for sheet_name in workbook.sheetnames:
+    # Select the sheet in the input workbook
+    sheet = workbook[sheet_name]
+
+    # Create a new sheet in the output workbook
+    output_sheet = output_workbook.create_sheet(title=sheet_name)
+
+    # Iterate through each row in the input sheet
+    for row in sheet.iter_rows(values_only=True):
+        # Write the row to the output sheet without escaping HTML
+        output_sheet.append(row)
+
+# Save the output workbook as HTML
+output_workbook.save("output.html")
