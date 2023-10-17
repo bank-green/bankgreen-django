78f9d6e39bc663495f2f1800a531839d8e0b91f9
ahmedlamidi <111273639+ahmedlamidi@users.noreply.github.com>
2023-10-17 22:52:14 +0000

    modified:   brand/admin.py 	renamed:    templates/admin/brand/brand/change_list_template.html -> brand/templates/change_list_template.html

--- brand/admin.py
+++ brand/admin.py
@@ -274,8 +274,7 @@ class InstitutionCredentials(admin.ModelAdmin):
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     form = CountriesWidgetOverrideForm
-
-    change_list_template = "admin/brand/brand/change_list_template.html"
+    change_list_template = "change_list_template.html"
 
     @admin.display(description="related datasources")
     def related_datasources(self, obj):
