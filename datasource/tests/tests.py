95cb831a6eb0ae6678cc15e283f6dac36afdd974
nikita-pardeshi-github <nikita.pardeshi.it@gmail.com>
2024-03-05 14:46:27 -0800

    Reverted previous changes and added command to load initial.json'

--- datasource/tests/tests.py
+++ datasource/tests/tests.py
@@ -310,39 +310,13 @@ class UsnicTestCase(TestCase):
 
 class FixtureLoadingTestCase(TestCase):
     def setUp(self):
-        """
-        This method will generate the sample_initial.json file which contains exactly one entry
-        for each module. The size of this file will be very small as compared to the original
-        initial.json
-        """
-
-        file = open("fixtures/initial/initial.json", "r")
-        json_file = json.load(file)
-
-        sample_initial_json = list({data["model"]: data for data in json_file}.values())
-
-        for data in sample_initial_json:
-            data["pk"] = 1
-            for field, field_val in data["fields"].items():
-                if isinstance(field_val, int):
-                    data["fields"][field] = 1
-
-        with open("fixtures/initial/sample_initial.json", "w") as file:
-            json.dump(sample_initial_json, file)
-
-    def tearDown(self):
-        """
-        Just remove the sample_initial.json file after executing the test case.
-        """
-        os.remove("fixtures/initial/sample_initial.json")
+        pass
 
     def test_can_import_fixtures(self):
         """
         Verify that it is always possible to import initial.json file
         """
         try:
-            call_command("loaddata", "fixtures/initial/sample_initial.json", verbosity=0)
-            exit_code = sys.exc_info()[0]
-            self.assertIsNone(exit_code)
+            call_command("loaddata", "fixtures/initial/initial.json", verbosity=0)
         except Exception as error:
             self.fail(error)
