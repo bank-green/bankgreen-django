ff7d135aadf923b1ebc59d34a4dc657fae6f92ae
Isaac Cowhey <isaaccowhey@Isaacs-MacBook-Air.local>
2024-04-02 21:55:20 -0700

    render rating

--- bankgreen/templatetags/bankgreen_tags.py
+++ bankgreen/templatetags/bankgreen_tags.py
@@ -9,7 +9,9 @@ register = template.Library()
 def render_revision_table(action_list):
     brand_id = action_list[0]['url'].split('/')[4]
     reversion_ids = [x.id + 1 for x in Version.objects.get_for_object_reference(Brand, brand_id)]
-    reversion_ratings = [Version.objects.get(id=reversion_id).field_dict['rating'] for reversion_id in reversion_ids]
+    reversion_ratings = [Version.objects.get(id=reversion_id).field_dict['rating'] for reversion_id in reversion_ids][::-1]
+
     for i in range(len(action_list)):
         action_list[i]['rating'] = reversion_ratings[i]
+
     return { 'action_list': action_list }
