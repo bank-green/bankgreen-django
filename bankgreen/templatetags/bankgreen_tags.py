9d900ef1e2b64a81a29b4797acd81439f1d2ef82
Isaac Cowhey <isaaccowhey@Isaacs-MacBook-Air.local>
2024-04-10 03:06:32 -0700

    update regex

--- bankgreen/templatetags/bankgreen_tags.py
+++ bankgreen/templatetags/bankgreen_tags.py
@@ -1,3 +1,5 @@
+import re
+
 from django import template
 from reversion.models import Version, Revision
 
@@ -7,9 +9,9 @@ register = template.Library()
 
 @register.inclusion_tag('admin/brand/brand/revision_table.html')
 def render_revision_table(action_list):
-    brand_id = action_list[0]['url'].split('/')[4]
+    brand_id = re.search(r'(?<=/admin/brand/brand/)(\d+)', action_list[0]['url']).group(0)
     reversion_ids = [x.id + 1 for x in Version.objects.get_for_object_reference(Brand, brand_id)]
-    reversion_ratings = [Version.objects.get(id=reversion_id).field_dict['rating'] for reversion_id in reversion_ids][::-1]
+    reversion_ratings = [Version.objects.get(id=reversion_id).field_dict.get('rating') for reversion_id in reversion_ids][::-1]
 
     for i in range(len(action_list)):
         action_list[i]['rating'] = reversion_ratings[i]
