46d826cbce085e9d8f16bb07179e4103e694861b
Isaac <BenBagBag@users.noreply.github.com>
2024-04-12 08:28:44 -0700

    Groot 137 revision history table (#160)
    * update object history
    
    * return rating changes
    
    * render rating
    
    * update regex
    
    * formatting
    
    ---------
    
    Co-authored-by: Isaac Cowhey <isaaccowhey@Isaacs-MacBook-Air.local>

--- bankgreen/settings.py
+++ bankgreen/settings.py
@@ -97,7 +97,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-            ]
+            ],
+            "libraries": {"bankgreen_tags": "bankgreen.templatetags.bankgreen_tags"},
         },
     }
 ]
