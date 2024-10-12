e4d52eeac053bd45cda2524578e6774c65c0e0fa
3DPros <altbillington@gmail.com>
2024-10-11 23:08:51 -0500

    review suggestions

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ app_name = "rest_api"
 urlpatterns = [
     path("", views.BrandSuggestionAPIView.as_view()),
     path("bank-contacts/", views.ContactView.as_view(), name="contacts"),
-    path("bank/", views.BrandsView.as_view(), name="bank"),
+    path("bank", views.BrandsView.as_view(), name="bank"),
 ]
