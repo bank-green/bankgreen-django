42af0effd468c8b1902c2ae3ae3b0afe896525aa
Alexander Sibhatu <sibhatualexander@gmail.com>
2025-02-22 13:29:28 +0000

    ALLOWED_HOSTS empty string if not set

--- bankgreen/settings.py
+++ bankgreen/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = (
-    os.environ.get("ALLOWED_HOSTS").split(" ")
+    os.environ.get("ALLOWED_HOSTS", "")
 )
 
 # Calendar URL
