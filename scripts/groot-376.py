151b76039ed1b87b4d1154a48663f3071a4ce272
Matthew C <crompton.dev@gmail.com>
2025-03-06 18:04:36 +0100

    Matthew/groot 312 dbg refreshes harvest data periodically for sfis (#204)
    * groot-312 Add script to refresh harvest data
    
    * groot-312 add logging
    
    * groot-312 fix failing formatting checks

--- scripts/groot-376.py
+++ scripts/groot-376.py
@@ -1,7 +1,8 @@
 from brand.models import *
 
-full_headlines = Commentary.objects.all().exclude(headline__isnull=True).exclude(headline='')
-full_subtitles = Commentary.objects.all().exclude(subtitle__isnull=True).exclude(subtitle='')
+
+full_headlines = Commentary.objects.all().exclude(headline__isnull=True).exclude(headline="")
+full_subtitles = Commentary.objects.all().exclude(subtitle__isnull=True).exclude(subtitle="")
 
 tags = {x.brand.tag for x in full_headlines}.union({x.brand.tag for x in full_subtitles})
 
@@ -9,6 +10,6 @@ for tag in tags:
     print(tag)
     print(f"HEADLINE: {Brand.objects.get(tag=tag).commentary.headline}")
     print(f"SUBTITLE: {Brand.objects.get(tag=tag).commentary.subtitle}")
-    print('----')
+    print("----")
 
-full_headlines.update(headline='')
+full_headlines.update(headline="")
