a6a27b42608f438e904fb6b224619980f21d4755
Matthew C <crompton.dev@gmail.com>
2025-07-22 07:09:03 -0700

    Matthew/groot 414 create and schedule script for updating uscaau bank (#219)
    * groot-414 Add harvest location endpoint to bgd
    
    * groot-414 Add command to refresh brand locations
    
    * groot-414 Fix url for harvest data fetching
    
    * groot-414 Fix filter query
    
    * groot-414 bugfix: fix typo
    
    * groot-414 Remove timeout for harvest location
    
    * groot-414 refactor: code cleanup
    
    * add harvest token to .env.template
    
    ---------
    
    Co-authored-by: Albert Carter <albert.r.carter@gmail.com>

---
+++ brand/management/commands/refresh_brand_locations.py
@@ -0,0 +1,55 @@
+from django.core.management.base import BaseCommand
+
+from brand.models import Brand, State
+from brand.utils.harvest_data import fetch_harvest_location_data
+
+
+"""
+    To be used with cron job, ran ever 3 month
+    please see GROOT-414 ticket for more context
+"""
+
+
+class Command(BaseCommand):
+    help = "For brands located in US/CA/AU, this refreshes their state information using harvest"
+    countries = ["AU", "US", "CA"]
+
+    def handle(self, *args, **options):
+        brands_by_country = {}
+
+        for country in self.countries:
+            brands_by_country[country] = Brand.objects.filter(countries__in=[country])
+        print(f"Initialized...")
+        for country, brands in brands_by_country.items():
+            for brand in brands:
+                print(f"Attempt for: {brand.tag}, {country}")
+                data = fetch_harvest_location_data(
+                    brand_tag=brand.tag,
+                    brand_url=brand.website,
+                    brand_country=country,
+                    brand_name=brand.name,
+                )
+
+                if isinstance(data, Exception):
+                    print(f"\tError: {brand.tag}, failed to fetch:", data)
+                    continue
+
+                location_data = data.get("location")
+                if not location_data:
+                    print(f"\tError: {brand.tag}: missing location data")
+                    continue
+
+                state_licensed = location_data.get("licensed_to_operate_in", [])
+                state_physical_branch = location_data.get("physical_branches", [])
+
+                for state_code in state_licensed:
+                    state = State.objects.filter(tag=state_code).first()
+                    if state:
+                        brand.state_licensed.add(state)
+
+                for state_code in state_physical_branch:
+                    state = State.objects.filter(tag=state_code).first()
+                    if state:
+                        brand.state_physical_branch.add(state)
+                print(f"\tCompleted: {brand.tag}, {country}")
+        print("Completed")
--- brand/utils/harvest_data.py
+++ brand/utils/harvest_data.py
@@ -12,7 +12,7 @@ import requests
 def fetch_harvest_data(
     brand_tag, brand_url="", brand_country="", brand_name=""
 ) -> Union[Dict, Exception]:
-    base_url = "https://bank.green/harvest"
+    base_url = "https://harvest.bank.green/harvest"
 
     params = {
         "bankTag": brand_tag,
@@ -30,6 +30,32 @@ def fetch_harvest_data(
         response = requests.get(
             url, headers={"Authorization": f"Token {settings.HARVEST_TOKEN}"}, timeout=600
         )
+
+        return response.json()
+    except Exception as e:
+        return e
+
+
+def fetch_harvest_location_data(
+    brand_tag, brand_url="", brand_country="", brand_name=""
+) -> Union[Dict, Exception]:
+    base_url = "https://harvest.bank.green/location"
+
+    params = {
+        "bankTag": brand_tag,
+        "bankUrl": brand_url,
+        "bankCountry": brand_country,
+        "bankName": brand_name,
+    }
+
+    # URL encode the parameters
+    encoded_params = urlencode(params, doseq=True)
+
+    url = f"{base_url}?{encoded_params}"
+
+    try:
+        response = requests.get(url, headers={"Authorization": f"Token {settings.HARVEST_TOKEN}"})
+        response.raise_for_status()
 
         return response.json()
     except Exception as e:
